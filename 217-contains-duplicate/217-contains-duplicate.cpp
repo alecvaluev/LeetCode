ead6fb091c56c14a5794098692d531d35c75db27
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-09-20 14:10:01 -0400

    Time: 216 ms (17.38%), Space: 52.2 MB (29.16%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         set<int> mapping;
-        set<int>::iterator iter;
         
         for(int i: nums){
-            iter = mapping.find(i);
-            if(iter != mapping.end()) return true;
-            else mapping.insert(i);
+            if(mapping.count(i) != 0) return true;
+            mapping.insert(i);
         }
         return false;
     }
