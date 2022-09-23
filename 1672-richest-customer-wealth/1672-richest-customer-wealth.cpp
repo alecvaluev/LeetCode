b75696945cd440e0e7d0f619635b474b6222e299
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-09-23 16:29:09 -0400

    Time: 10 ms (44.82%), Space: 7.9 MB (51.06%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        // solution O(n^2)
+        int rows = accounts.size();
+        int cols = accounts[0].size();
+        int reachest = 0;
+        
+        for(int i = 0; i < rows; i++){
+            int sum = 0;
+            for(int j = 0; j < cols; j++){
+                sum += accounts[i][j];        
+            }
+            if(sum > reachest) reachest = sum;
+        }
+        return reachest;
+    }
+};
