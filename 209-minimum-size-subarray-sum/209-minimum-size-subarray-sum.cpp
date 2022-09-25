91e42e6a3f19b873c925d83c5fabad06bc7f855e
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-09-25 16:50:01 -0400

    Time: 120 ms (8.01%), Space: 28.1 MB (99.07%) - LeetHub

---
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        //brutal force - O(nk) - iterate thrtough vector and expand window until its >= target
+        
+        //optimized - O(n)
+        int winStart = 0,
+            n = nums.size(),
+            minLength = n + 1,
+            sum = 0;
+        
+        for(int winEnd = 0; winEnd < nums.size(); winEnd++){
+            sum += nums[winEnd];
+            
+            while(sum >= target){
+                minLength = min(minLength, winEnd - winStart + 1);
+                sum -= nums[winStart];
+                winStart++;
+            }
+        }
+        return (minLength == n + 1)? 0 : minLength;
+    }
+};
