ab978e009cd72e734eceec2e3d1d016c0b654bd4
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-09-26 22:32:20 -0400

    Time: 13 ms (31.55%), Space: 7.9 MB (7.67%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        vector<char> newS;
+        
+        for(auto& c: s){
+            if(isalnum(c)) newS.push_back(tolower(c));
+        }
+        for(int i = 0, j = newS.size() - 1; i < j; i++, j--){
+            if(newS[i] != newS[j]) return false;
+        }
+        return true;
+    }
+};
