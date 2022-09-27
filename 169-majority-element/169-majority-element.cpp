f7d272e38e22225b012d88d129fc54918ca84c89
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-09-26 21:40:13 -0400

    Time: 35 ms (48.42%), Space: 19.6 MB (14.39%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,10 +1,26 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        //use map to count frequencies O(n)
+        //use map to count frequencies O(n) 
         //sort -> count the middle and that's the answer
         
+        /*
         sort(nums.begin(), nums.end());
         return nums[nums.size() / 2];
+        */
+        
+        //candidate element O(n)
+        int candidate = nums[0],
+            count = 1;
+        
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] == candidate) count++;
+            else if (count == 0){
+                candidate = nums[i];
+                count = 1;
+            }
+            else count--;
+        }
+        return candidate;
     }
 };
