c6cdc62c1c62523f252e471e586032652804fa56
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-10-21 00:17:16 -0400

    Time: 43 ms (77.47%), Space: 13.9 MB (49.60%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -3,6 +3,9 @@ class Solution:
         count = 0
         
         while n > 0:
-            count += n % 2
-            n = n >> 1
+        #    count += n % 2
+        #    n = n >> 1
+        # Alternative( a bit faster - skips all zeros)
+             n = n & (n - 1)
+             count += 1
         return count
