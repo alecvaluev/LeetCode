fa28b3c8ca2558fd10d5048218bfaade014c30b9
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-10-30 17:21:17 -0400

    Time: 62 ms (69.71%), Space: 13.8 MB (65.24%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
@@ -2,12 +2,29 @@ class Solution:
     def specialArray(self, nums: List[int]) -> int:
         n = len(nums)
         
-        for i in range(n + 1):
-            total = 0
-            for num in nums:
-                if num >= i:
-                    total += 1
-            if total == i:
-                return i
+        # O(n^2)
+        #for i in range(n + 1):
+        #    total = 0
+        #    for num in nums:
+        #        if num >= i:
+        #            total += 1
+        #    if total == i:
+        #        return i
         
+        # log(n)
+        nums.sort()
+        
+        l, r = 0, n - 1
+        while l <= r:
+            mid = l + (r - l) //2
+            
+            x = n - mid
+            if nums[mid] >= x:
+                if mid == 0 or nums[mid - 1] < x:
+                    return x
+                else:
+                    r = mid - 1
+            else:
+                l = mid + 1
+                
         return -1
