5fbc12ca7878904014378fa679bb52da29f15934
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-10-30 17:59:21 -0400

    Time: 52 ms (64.12%), Space: 13.8 MB (95.92%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -1,6 +1,7 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        
+        #brute for loop from 0 to num => O(sqrt(n)) 
+        #log(n)
         l, r = 0, num
         while l <= r:
             mid = l + (r - l) // 2
