3dce3bdb2a7bf497a1e94706642f4833f7a73ec0
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-10-31 16:01:44 -0400

    Time: 164 ms (27.67%), Space: 14.2 MB (53.69%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,9 +1,26 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        noteDict = Counter(ransomNote)
-        magDict = Counter(magazine)
+        # space - O(n*m), time O(n + m)
+        #noteDict = Counter(ransomNote)
+        #magDict = Counter(magazine)
+        # 
+        #for letter, count in noteDict.items():
+        #    if not(letter in magDict and count <= magDict[letter]):
+        #        return False
+        #return True
         
-        for letter, count in noteDict.items():
-            if not(letter in magDict and count <= magDict[letter]):
+        # space O(m)
+        dic = {}
+        for char in magazine:
+            if char not in dic:
+                dic[char] = 1
+            else:
+                dic[char] += 1
+        
+        for char in ransomNote:
+            if char not in dic or dic[char] == 0:
                 return False
+            else:
+                dic[char] -= 1
+        
         return True
