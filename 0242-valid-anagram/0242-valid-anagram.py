308d73d89c3bf880599accef289802ee7ec7ea28
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-10-31 16:47:12 -0400

    Time: 41 ms (98.34%), Space: 14.5 MB (67.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,4 +3,17 @@ class Solution:
         if len(s) !=len(t):
             return False
         
-        return Counter(s) == Counter(t)
+        #return Counter(s) == Counter(t)
+        counterS, counterT = {}, {}
+        
+        for i in range(len(s)):
+            counterS[s[i]] = 1 + counterS.get(s[i], 0)
+            counterT[t[i]] = 1 + counterT.get(t[i], 0)
+        
+        for c in counterS:
+            if counterS[c] != counterT.get(c, 0):
+                return False
+        
+        return True
+
+            
