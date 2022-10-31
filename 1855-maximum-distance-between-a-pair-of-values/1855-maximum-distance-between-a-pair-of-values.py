ea5cf58f61304949b365cb3c287023cf827185d6
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-10-31 15:24:20 -0400

    Time: 7422 ms (5.03%), Space: 32 MB (64.15%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.py
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.py
@@ -11,14 +11,32 @@ class Solution:
         #return maxD
         
         # 2 pointers O(n + m)
-        i = j = 0
+        #i = j = 0
+        #maxD = 0
+        # 
+        #while i < len(nums1) and j < len(nums2):
+        #    if nums1[i] <= nums2[j]:
+        #        maxD = max(maxD, j - i)
+        #        j += 1
+        #    else:
+        #        i += 1
+        #
+        #return maxD
+    
+        # binary search on 2nd array
         maxD = 0
         
-        while i < len(nums1) and j < len(nums2):
-            if nums1[i] <= nums2[j]:
-                maxD = max(maxD, j - i)
-                j += 1
-            else:
-                i += 1
-        
+        for i, num in enumerate(nums1):
+            
+            l, r  = 0, len(nums2) - 1
+
+            while l <= r:
+                mid = l + (r - l) // 2
+
+                if nums2[mid] >= num:
+                    maxD = max(maxD, mid - i)
+                    l = mid + 1
+                else:
+                    r = mid - 1
+                    
         return maxD
